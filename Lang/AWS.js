6375be2f53db56d08531bdd182082b943503b4bd
abhishekdev073 <abhishekdev073@gmail.com>
2021-03-11 17:49:55 +0530

    aws

---
+++ Lang/AWS.js
@@ -0,0 +1,22 @@
+/*
+SAML
+
+1 AWS (Amazon web services) 30+
+2 Region :Most services are region scoped -e
+   Every region has many cluster of data center(AZ)
+   Availability zone :- Each region min 3-6 AZ
+   One AZ is down we can launch another
+3 IAM(Identity Access management) : User/Groups/Roles/ security, Center of  aws
+  User/Groups/Roles
+  We write policies in JSON. What user can and can not access
+  One IAM Role per Application prefer
+  Never use Root account or don't share with anyone
+4 EC2 : we create server instance 
+    To connect server : putty window < 10
+    SSH for Mac/Window 10/Linux
+    EC2 connect for all from browser 
+ 
+  
+
+
+*/
